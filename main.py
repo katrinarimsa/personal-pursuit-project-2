e49226eeb38354116a9eb15b145c437fa630bff7
katrinarimsa <125032516+katrinarimsa@users.noreply.github.com>
2023-02-10 13:06:13 +0100

    notes 1

---
+++ main.py
@@ -0,0 +1,34 @@
+a = 5
+b = 3
+print(a+b)
+a = 10
+print(a+b)
+a = "katrina" #string
+print(a)
+print(a,b)
+b = "3"
+print(a+b)
+#types of variables
+#int - integer
+#float - decimals
+a_int = 5
+a_float = 5.045
+print(int(a_float))
+
+print(type(a_float)) #tells the type of variable
+
+c = 7.0
+print(type(c)) #if you have .0 its a float
+c=7
+print(c/2)
+print(type(c/2))
+print(type(c/1)) #because of division, it immediately counts is as a float
+
+#variable type - string (smt that does not have a numerical value, eg x)
+string = "pola"
+print(string + a)
+print(string + " and " + a)
+
+#gethub
+
+
